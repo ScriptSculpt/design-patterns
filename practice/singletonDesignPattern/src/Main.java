725e1b14cb565eb2c9c0734c9f572b315df55412
Aastha Singh <singhaastha1202@gmail.com>
2026-06-07 17:38:33 +0530

    Singleton Design Pattern with thread safety

--- practice/singletonDesignPattern/src/Main.java
+++ practice/singletonDesignPattern/src/Main.java
@@ -2,8 +2,8 @@ package practice.singletonDesignPattern.src;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton s1 = Singleton.getInstance();
-        Singleton s2 = Singleton.getInstance();
+        Singleton s1 = Singleton.getInstanceThreadSafeFast();
+        Singleton s2 = Singleton.getInstanceThreadSafeFast();
 
         System.out.println("Are S1 and S2 same? : " + (s1 == s2));
     }
--- practice/singletonDesignPattern/src/Singleton.java
+++ practice/singletonDesignPattern/src/Singleton.java
@@ -2,7 +2,7 @@ package practice.singletonDesignPattern.src;
 
 public class Singleton {
 
-    private static Singleton singleton = null;
+    private static Singleton INSTANCE = null;
 
     private Singleton() {
         System.out.println("Singleton class constructor called");
@@ -10,9 +10,31 @@ public class Singleton {
 
     // Not thread safe
     public static Singleton getInstance() {
-        if(singleton == null) {
-            singleton = new Singleton();
+        if(INSTANCE == null) {
+            INSTANCE = new Singleton();
         } 
-        return singleton;
+        return INSTANCE;
     }
+
+    // Thread safe but slow
+    public static synchronized Singleton getInstanceThreadSafeSlow() {
+        if(INSTANCE == null) {
+            INSTANCE = new Singleton();
+        } 
+        return INSTANCE;
+    }
+
+    // Thread safe with double check locking, fast
+    public static Singleton getInstanceThreadSafeFast() {
+        if(INSTANCE == null) {
+            synchronized(Singleton.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
 }
